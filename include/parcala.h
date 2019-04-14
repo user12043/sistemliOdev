03ea0ad4e1eee552afa047a87e3f69e5385c343d
noaahhh <nuh_yrdsvn@outlook.com>
2019-04-15 00:37:58 +0300

    arsiv okunup dosyalar yeniden oluşturuldu

--- include/parcala.h
+++ include/parcala.h
@@ -5,6 +5,22 @@
 #ifndef SISTEMLIODEV_PARCALA_H
 #define SISTEMLIODEV_PARCALA_H
 
+#include "stdlib.h"
+#include "stdio.h"
+#define BUFFERSIZE 6553600 + 500 //32 file * 200 mb + first part approximately
+
+FILE *fp;//file pointer for arshive file
+char buffer[BUFFERSIZE]; //the buffer for arshive file 
+int  fp2; // file pointer for extracting files
+int sizes[204800];//file sizes integer max 200 mb
+char permisions[5];//permissions octal array 
+struct file
+{
+    char name[100];
+    char permissions[5];
+    char size[1000];
+    char text[10000];
+}files[32];//max 32 file
 void parcala(char *archiveFile, char *outputFolder);
 
 #endif //SISTEMLIODEV_PARCALA_H
--- src/parcala.c
+++ src/parcala.c
@@ -1,10 +1,120 @@
 //
 // Created by user12043 on 04.04.2019.
-//
 
 #include <parcala.h>
 #include <stdio.h>
+#include "stdlib.h"
+#include "fcntl.h"
+#include "unistd.h"
+#include "string.h"
+#include "sys/stat.h"
 
 void parcala(char *archiveFile, char *outputFolder) {
-    printf("%s\n", "parcalanacak");
+ 
+  // char * home = getenv("HOME");
+//printf("home_path=%s\n",home);
+
+//char * filepath = "arsiv.sau";
+//char * pt;
+   //pt = realpath(filepath,NULL);
+    //printf("real_path:%s\n",pt);
+    
+    
+    fp = fopen(archiveFile, "r");
+
+    if (fp == NULL)
+    {
+        fprintf(stderr, "this file is not occured!");
+        exit(1);
+    }
+
+    fread(buffer, 10, BUFFERSIZE, fp);
+    
+    
+    char sub[10];
+    //ilk 10 bit okunuyor ve bir array a atanıyor     
+    for (int i = 0; i < 10; i++)    sub[i] = buffer[i];
+    
+    int descSize = atoi(sub);//array deki string integer a çevriliyor
+    
+    //printf("%d", descSize);
+    
+    int partIndex = 0;
+    int i = 11;
+    int fileCount = 0;
+    int descCursor = 11;
+    int textCursor = descSize;
+    int objIndex = 0; 
+    //birinci bölüm | nesnelere atama 
+    for (int i = 11; i < descSize; i++){
+        //switch-case 
+        if (buffer[i] == '|')
+        {
+            fileCount++;
+            int partNo = 1;
+            partIndex = 0;
+
+
+            for (int j = descCursor; j < i; j++)
+            {
+                if (buffer[j] == ','){
+                    partNo++;
+                    j++;
+                    partIndex = 0;
+                    descCursor++;
+                }
+                
+                if (partNo == 1){
+                    //name
+                    files[objIndex].name[partIndex] = buffer[descCursor];
+                    partIndex++;
+                    descCursor++;
+                }
+                else if (partNo == 2){
+                    //permission
+                    files[objIndex].permissions[partIndex] = buffer[descCursor];
+                    partIndex++;
+                    descCursor++;
+                }
+                else if (partNo == 3){
+                    //Size
+                    files[objIndex].size[partIndex] = buffer[descCursor];
+                    partIndex++;
+                    descCursor++;
+                }
+            }
+            descCursor = i + 1;
+            objIndex++;
+        }
+    }
+
+    int sayac = descSize;
+    for (int i = 0; i < fileCount; i++) //text selection part
+    {
+        for (int j = 0; j < atoi(files[i].size); j++)
+        {
+            files[i].text[j] = buffer[j + sayac];
+        }
+        sayac += atoi(files[i].size);
+    }
+
+
+
+    for (int i = 0; i < fileCount; i++){
+
+    // printf("name: %s   Permission: %s   Size: %s\n Text: %s \n\n", files[i].name, files[i].permissions, files[i].size, files[i].text);
+    
+    sizes[i] = atoi(files[i].size);
+    permisions[i]=strtol(files[i].permissions,NULL,8);
+    
+  //  printf(" permission = %d \n",permisions[i]);
+    
+    fp2 = open(files[i].name,O_CREAT | O_WRONLY | O_TRUNC,permisions[i]);
+    write(fp2,files[i].text,sizes[i]);
+    
+
+    close(fp2);
+    
+    }
+    
 }

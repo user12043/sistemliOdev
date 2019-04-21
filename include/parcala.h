c7f207c3d0109682330c5f91417da37a5583d327
user12043 <user12043@hotmail.com>
2019-04-21 10:47:27 +0300

    reformatted code

--- include/parcala.h
+++ include/parcala.h
@@ -7,22 +7,22 @@
 
 #include "stdlib.h"
 #include "stdio.h"
+
 #define BUFFERSIZE 6553600 + 500 //32 file * 200 mb + first part approximately
 
 FILE *fp;//file pointer for arshive file
-char buffer[BUFFERSIZE]; //the buffer for arshive file 
-int  fp2; // file pointer for extracting files
+char buffer[BUFFERSIZE]; //the buffer for archive file
+int fp2; // file pointer for extracting files
 int sizes[204800];//file sizes integer max 200 mb
 int permisions[5];//permissions octal array 
 char fullpath[250] = "";
-struct file
-{
+struct file {
     char name[100];
     char permissions[5];
     char size[204800];
     char text[204800];
-    
-}files[32];//max 32 file
+
+} files[32];//max 32 file
 void parcala(char *archiveFile, char *outputFolder);
 
 #endif //SISTEMLIODEV_PARCALA_H
--- src/parcala.c
+++ src/parcala.c
@@ -13,74 +13,68 @@
 
 void parcala(char *archiveFile, char *outputFolder) {
 
-     char output[200]="";
-    
-        DIR *output_dir = opendir(outputFolder);
-        if (output_dir == NULL) {
-            mkdir(outputFolder, 0644);
-        }
-        strcat(output, outputFolder);
-        strcat(output, "/");
-    
+    char output[200] = "";
 
-    
+    DIR *output_dir = opendir(outputFolder);
+    if (output_dir == NULL) {
+        mkdir(outputFolder, 0644);
+    }
+    strcat(output, outputFolder);
+    strcat(output, "/");
+
+
     fp = fopen(archiveFile, "r");
 
-    if (fp == NULL)
-    {
+    if (fp == NULL) {
         fprintf(stderr, "this file is not occured!");
         exit(1);
     }
 
     fread(buffer, 10, BUFFERSIZE, fp);
-    
-    
+
+
     char sub[10];
     //ilk 10 bit okunuyor ve bir array a atanıyor     
-    for (int i = 0; i < 10; i++)    sub[i] = buffer[i];
-    
+    for (int i = 0; i < 10; i++) sub[i] = buffer[i];
+
     int descSize = atoi(sub);//array deki string integer a çevriliyor
-    
+
     //printf("%d", descSize);
-    
+
     int partIndex = 0;
     int i = 11;
     int fileCount = 0;
     int descCursor = 11;
     int textCursor = descSize;
-    int objIndex = 0; 
+    int objIndex = 0;
     //birinci bölüm | nesnelere atama 
-    for (int i = 11; i < descSize; i++){
+    for (int i = 11; i < descSize; i++) {
         //switch-case 
-        if (buffer[i] == '|')
-        {
+        if (buffer[i] == '|') {
             fileCount++;
             int partNo = 1;
             partIndex = 0;
 
 
-            for (int j = descCursor; j < i; j++)
-            {
-                if (buffer[j] == ','){
+            for (int j = descCursor; j < i; j++) {
+                if (buffer[j] == ',') {
                     partNo++;
                     j++;
                     partIndex = 0;
                     descCursor++;
                 }
-                
-                if (partNo == 1){
+
+                if (partNo == 1) {
                     //name
                     files[objIndex].name[partIndex] = buffer[descCursor];
                     partIndex++;
                     descCursor++;
-                }
-                else if (partNo == 2){
+                } else if (partNo == 2) {
                     //permission
                     files[objIndex].permissions[partIndex] = buffer[descCursor];
                     partIndex++;
                     descCursor++;
-                }
-                else if (partNo == 3){
+                } else if (partNo == 3) {
                     //Size
                     files[objIndex].size[partIndex] = buffer[descCursor];
                     partIndex++;
@@ -95,38 +89,35 @@ void parcala(char *archiveFile, char *outputFolder) {
     int sayac = descSize;
     for (int i = 0; i < fileCount; i++) //text selection part
     {
-        for (int j = 0; j < atoi(files[i].size); j++)
-        {
+        for (int j = 0; j < atoi(files[i].size); j++) {
             files[i].text[j] = buffer[j + sayac];
         }
         sayac += atoi(files[i].size);
     }
 
 
+    for (int i = 0; i < fileCount; i++) {
 
-    for (int i = 0; i < fileCount; i++){
+        // printf("name: %s   Permission: %s   Size: %s\n Text: %s \n\n", files[i].name, files[i].permissions, files[i].size, files[i].text);
 
-    // printf("name: %s   Permission: %s   Size: %s\n Text: %s \n\n", files[i].name, files[i].permissions, files[i].size, files[i].text);
-    
-    sizes[i] = atoi(files[i].size);
-    permisions[i]=strtol(files[i].permissions,NULL,8);
+        sizes[i] = atoi(files[i].size);
+        permisions[i] = strtol(files[i].permissions, NULL, 8);
 
 
-    strcpy(fullpath,output);
-    strcat(fullpath,files[i].name);
+        strcpy(fullpath, output);
+        strcat(fullpath, files[i].name);
 
-    fp2 = open(fullpath,O_CREAT | O_WRONLY | O_TRUNC,permisions[i]);
-    
-    if (fp < 0)
-    {
-       fprintf(stderr, "Error: %s", strerror(errno));
-        exit(1);
+        fp2 = open(fullpath, O_CREAT | O_WRONLY | O_TRUNC, permisions[i]);
+
+        if (fp < 0) {
+            fprintf(stderr, "Error: %s", strerror(errno));
+            exit(1);
+        }
+        write(fp2, files[i].text, sizes[i]);
+
+
+        close(fp2);
+
     }
-    write(fp2,files[i].text,sizes[i]);
-    
 
-    close(fp2);
-    
-    }
-    
 }
